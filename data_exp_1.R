2d532adca738c2cf81a18f1cd6308ef74653bdac
Jan Moffett <49538827+JanLMoffett@users.noreply.github.com>
2022-12-14 07:23:06 -0500

    working on script for cleaning/processing dataset

--- data_exp_1.R
+++ data_exp_1.R
@@ -1,22 +1,42 @@
 
-library(baseballr)
+#load packages
 library(tidyverse)
-library(maps)
-library(mapdata)
-library(usmap)
-library(rworldmap)
-library(sf)
 library(devtools)
 library(lubridate)
 
+#dataviz code from my github
 source_url("https://raw.githubusercontent.com/JanLMoffett/datavizExtras/master/colorConstants.R")
 source_url("https://raw.githubusercontent.com/JanLMoffett/datavizExtras/master/extraThemes.R")
 source_url("https://raw.githubusercontent.com/JanLMoffett/datavizExtras/master/datavizExtras.R")
 
 #setwd("~/Projects/GithubRepositories/F1_Data")
 
-drv <- read.csv("datasets/drivers.csv")
-View(drv)
+#names of tables in dataset:
+tbl_names <- c("circuits", "constructor_results", "constructor_standings", 
+                "constructors", "driver_standings", "drivers", "lap_times",
+                "pit_stops", "qualifying", "races", "results", "seasons", 
+                "sprint_results", "status")
+
+#make into filepaths
+tbl_names <- paste0("datasets/", tbl_names, ".csv")
+
+#list to hold tables
+tbl_list <- list(circuits, 
+     constructor_results = NA,
+     constructor_standings = NA,
+     constructors = NA,
+     driver_standings = NA,
+     drivers = NA,
+     lap_times = NA,
+     pit_stops = NA,
+     qualifying = NA,
+     races = NA,
+     results = NA,
+     seasons = NA,
+     sprint_results = NA,
+     status = NA)
+
+
 
 drv %>% pull(dob)
 
@@ -24,7 +44,7 @@ drv <- drv %>% filter(year(dob) > 1980)
 
 drv <- drv %>% arrange(dob)
 
-drv %>% pull(code)
+drv %>% pull(code) %>% unique()
 
 keep_codes <- c("ALO", "HAM", "HUL", "RIC", "BOT", 
                 "VER", "MAG", "GIO", "SAI", "DEV", 
